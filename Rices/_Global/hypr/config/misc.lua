4e4bf72060cd296396f26a2404e2eecc75f904b9
Koshki <137661698+Teamkel01@users.noreply.github.com>
2026-09-07 19:47:01 +0100

    Add files via upload

--- Rices/_Global/hypr/config/misc.lua
+++ Rices/_Global/hypr/config/misc.lua
@@ -30,7 +30,9 @@ hl.config({
 })
 
 
+hl.env("XCURSOR_THEME", "Adwaita")
 hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_THEME", "Adwaita")
 hl.env("HYPRCURSOR_SIZE", "24")
 
 local suppressMaximizeRule = hl.window_rule({
